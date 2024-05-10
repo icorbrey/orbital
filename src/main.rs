ac433c335571ec3e5041db08e106ef11010756c8
Isaac <icorbrey@gmail.com>
2024-05-10 14:23:55 -0400

    chore: Refactor

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,10 @@ mod cleanup {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins((
-            InputManagerPlugin::<Action>::default(),
-            EditorPlugin::new(),
-            RngPlugin::new(),
-        ))
+        .add_plugins((EditorPlugin::new(), RngPlugin::new()))
         .add_plugins((BodyPlugin, CameraPlugin, MotionPlugin, PlayerPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, spawn_random_body)
         .run();
-}
-
-#[derive(Actionlike, Component, PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
-enum Action {
-    SpawnRandomBody,
 }
 
 fn setup(
@@ -37,13 +27,6 @@ fn setup(
     mut global_rng: ResMut<GlobalRng>,
     mut commands: Commands,
 ) {
-    let input_map = InputMap::new([(Action::SpawnRandomBody, KeyCode::Space)]);
-
-    commands.spawn((
-        Name::new("Input Manager"),
-        cleanup::OnExit,
-        InputManagerBundle::with_map(input_map),
-    ));
     commands.spawn((
         Name::new("Random Number Generator"),
         cleanup::OnExit,
@@ -73,27 +56,4 @@ fn setup(
             ..default()
         },
     ]);
-}
-
-fn spawn_random_body(
-    action_state: Query<&ActionState<Action>>,
-    mut ev_spawn_body: EventWriter<SpawnBodyEvent>,
-    mut rng: Query<&mut RngComponent>,
-) {
-    let action_state = action_state.single();
-    let mut rng = rng.single_mut();
-
-    if action_state.just_pressed(&Action::SpawnRandomBody) {
-        ev_spawn_body.send(SpawnBodyEvent {
-            velocity: 100.0 * Vec2::new(rng.f32() - 0.5, rng.f32() - 0.5),
-            position: 200.0 * Vec2::new(rng.f32() - 0.5, rng.f32() - 0.5),
-            mass: 1000000.0 * rng.f32(),
-            color: Color::Hsla {
-                saturation: 0.2 + 0.8 * rng.f32(),
-                lightness: 0.2 + 0.8 * rng.f32(),
-                hue: 360.0 * rng.f32(),
-                alpha: 1.0,
-            },
-        });
-    }
 }
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,11 +1,13 @@
 pub mod prelude;
 
-use crate::prelude::*;
+use crate::prelude::{body::*, *};
 
 pub(crate) struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_plugins(InputManagerPlugin::<PlayerAction>::default())
+            .add_systems(Startup, setup)
+            .add_systems(Update, spawn_random_body);
     }
 }
 
@@ -25,4 +27,27 @@ fn setup(mut commands: Commands) {
         cleanup::OnExit,
         InputManagerBundle::with_map(input_map),
     ));
+}
+
+fn spawn_random_body(
+    action_state: Query<&ActionState<PlayerAction>>,
+    mut ev_spawn_body: EventWriter<SpawnBodyEvent>,
+    mut rng: Query<&mut RngComponent>,
+) {
+    let action_state = action_state.single();
+    let mut rng = rng.single_mut();
+
+    if action_state.just_pressed(&PlayerAction::SpawnRandomBody) {
+        ev_spawn_body.send(SpawnBodyEvent {
+            velocity: 100.0 * Vec2::new(rng.f32() - 0.5, rng.f32() - 0.5),
+            position: 200.0 * Vec2::new(rng.f32() - 0.5, rng.f32() - 0.5),
+            mass: 1000000.0 * rng.f32(),
+            color: Color::Hsla {
+                saturation: 0.2 + 0.8 * rng.f32(),
+                lightness: 0.2 + 0.8 * rng.f32(),
+                hue: 360.0 * rng.f32(),
+                alpha: 1.0,
+            },
+        });
+    }
 }
